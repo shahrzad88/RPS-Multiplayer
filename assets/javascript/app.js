de4739506c1ba6f8ec74d13ec2783130bf119c1b
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2018-11-07 23:52:38 -0500

    Added firebase initializations.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDVLtZZu8_iMCvpktQvOIKGFsl-R1Kgf1E",
+    authDomain: "rps-multiplayer-ddfe5.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-ddfe5.firebaseio.com",
+    projectId: "rps-multiplayer-ddfe5",
+    storageBucket: "rps-multiplayer-ddfe5.appspot.com",
+    messagingSenderId: "758445122440"
+  };
+firebase.initializeApp(config);
